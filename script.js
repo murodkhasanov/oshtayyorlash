6036f05afa360f95fbc7f916b75e765c63634894
murodkhasanov <tt1953385@gmail.com>
2023-05-13 17:54:52 +0500

    osh

---
+++ script.js
@@ -0,0 +1,73 @@
+
+function getMasalliq() {
+    let mahsul = "Osh tayyorlaymiz"
+    console.log(mahsul);
+}
+
+function getZigir() {
+    let zigir = "Zig'ir moy solinadi"
+    console.log(zigir);
+}
+
+function getGosht() {
+    let gosht = "Go'sht solinadi"
+    console.log(gosht);
+}
+
+function getPiyoz() {
+    let piyoz = "Piyoz solinadi"
+    console.log(piyoz);
+}
+
+function getSabzi() {
+    let sabzi = "Sabzi solinadi"
+    console.log(sabzi);
+}
+
+function getGuruch() {
+    let guruch = "Guruch solinadi"
+    console.log(guruch);
+}
+
+function getDaqiqa() {
+    let daqiqa = "20 daqiqa dimlanadi"
+    console.log(daqiqa);
+}
+
+function getOsh() {
+    let osh = "Osh tayyor"
+    console.log(osh);
+}
+
+setTimeout( () => {
+    getMasalliq()},2000
+)
+
+setTimeout( () => {
+    getZigir()},4000
+)
+
+setTimeout( () => {
+    getGosht()},6000
+)
+
+setTimeout( () => {
+    getPiyoz()},8000
+)
+
+setTimeout( () => {
+    getSabzi()},10000
+)
+
+setTimeout( () => {
+    getGuruch()},12000
+)
+
+setTimeout( () => {
+    getDaqiqa()},14000
+)
+
+setTimeout( () => {
+    getOsh()},16000
+)
+        
